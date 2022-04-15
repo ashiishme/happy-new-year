d4040c226bd7bd9a51e8c1a468c6dfac41113ba0
ashiishme <iamashiishy@gmail.com>
2022-04-15 21:33:28 +0000

    Deploying to gh-pages from  @ 9387900ec93d1911651a74a98bd1c1b666362fd8 🚀

---
+++ _next/static/6Qf1LJ3JawceLxkAqxWt8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d37dbec3d3a50fb7.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
