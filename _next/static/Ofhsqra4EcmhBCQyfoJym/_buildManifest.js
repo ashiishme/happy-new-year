158b5f6b3a35004b79b77def9e5a004c03a70fce
ashiishme <iamashiishy@gmail.com>
2022-04-15 20:17:34 +0200

    Updates

---
+++ _next/static/Ofhsqra4EcmhBCQyfoJym/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7cc0db81609d595d.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
